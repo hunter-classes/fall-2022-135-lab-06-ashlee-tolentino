a3e9eff8d915413631fb221453449b3a72e9d2cb
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-13 14:11:04 -0400

    shiftChar function draft done

--- caesar.cpp
+++ caesar.cpp
@@ -4,11 +4,27 @@ Course: CSCI-135
 Instructor: Michael Zamansky
 Assignment: Lab6
 
-This program...
+This program contains functions implementing Caesar cipher encryption.
 */
 
 #include <iostream>
-#include "funcs.h"
+#include <string>
+#include <cctype>
+#include "caesar.h"
 
-// add functions here
+// A helper function to shift one character by rshift
+char shiftChar(char c, int rshift) // rshift: 0 <= rshift <= 25
+{
+  char result;
+  result = c + rshift;
+  return result;
+}
 
+
+/*
+// Caesar cipher encryption
+std::string encryptCaesar(std::string plaintext, int rshift)
+{
+  return "hi";
+}
+*/
