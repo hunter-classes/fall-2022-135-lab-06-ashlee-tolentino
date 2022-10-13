04be163a548b8b042f77df577a9f843b8a5073d8
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-13 14:13:01 -0400

    added shiftChar example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab6
+
+This program contains functions implementing Caesar cipher encryption.
+*/
+
 #include <iostream>
-
-
-#include "funcs.h"
+#include <string>
+#include <cctype>
+#include "caesar.h"
 
 int main()
 {
+  char result = shiftChar('W', 5);
+  std::cout << result << std::endl;
+  
   return 0;
 }
