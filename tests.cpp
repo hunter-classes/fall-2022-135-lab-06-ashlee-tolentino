5b1f882636582748996e309d796448341ed23c24
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-16 23:54:14 -0400

    finished all tests

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,31 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
-// add your tests here
+TEST_CASE("shiftChar"){
+  CHECK(shiftChar('W', 5 == 'B'));
+  CHECK(shiftChar('o', 10) == 'y');
+  CHECK(shiftChar('!', 15) == '!');
+}
+
+TEST_CASE("Caesar Encryption"){
+  CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+  CHECK(encryptCaesar("Hello, World!", 10) == "Rovvy, Gybvn!");
+}
+
+TEST_CASE("Vigenere Encryption"){
+  CHECK(encryptVigenere("Way to Go!", "rain") == "Nag gf Gw!");
+  CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
+}
+
+TEST_CASE("Caesar Decryption"){
+  CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
+  CHECK(decryptCaesar("Bfd yt Lt!", 5) == "Way to Go!");
+}
+
+TEST_CASE("Vigenere Decryption"){
+  CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
+  CHECK(decryptVigenere("Nag gf Gw!", "rain") == "Way to Go!");
+}
